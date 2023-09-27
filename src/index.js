60ba9c34fdbdb848b1bc1ab3c20551e91df1cdcf
gabrielSanovis <9novis9@gmail.com>
2023-09-27 20:40:03 -0300

    Tree router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './routes/root';
+import ErrorPage from './error-page';
+import SignIn from './routes/signIn';
+import { SignUp } from './routes/signUp';
+import { Dashboard } from './routes/Dashboard';
+import { IncomeExpense } from './routes/IncomeExpense';
+import { Investment } from './routes/Investment';
+import { Budget } from './routes/Budget';
+import { Notification } from './routes/Notification';
+import { App } from './routes/App';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "sign-in",
+        element: <SignIn />,
+      },
+      {
+        path: "sign-up",
+        element: <SignUp />,
+      },
+      {
+        path: "organiza",
+        element: <App />,
+        children: [
+          {
+            path: "dashboard",
+            element: <Dashboard />,
+          },
+          {
+            path: "income-and-expense",
+            element: <IncomeExpense />,
+          },
+          {
+            path: "investment",
+            element: <Investment />,
+          },
+          {
+            path: "budget",
+            element: <Budget />,
+          },
+          {
+            path: "notification",
+            element: <Notification />,
+          },
+        ]
+      }
+    ]
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
