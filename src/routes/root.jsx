27c5e94045cc028b47a9ff06ad15e94505ecdbb5
Gabriel Santos <101470400+gabrielSanovis@users.noreply.github.com>
2023-09-29 15:45:43 -0300

    Update root.jsx

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Root() {
     return (
@@ -53,9 +53,6 @@ export default function Root() {
                         </li>
                     </ul>
                 </nav>
-            </div>
-            <div id="detail">
-                <Outlet />
             </div>
         </>
     );
