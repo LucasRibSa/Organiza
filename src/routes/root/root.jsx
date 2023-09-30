d63b2edb9aac10abc2e2880873c202e3d699d2ed
gabrielSanovis <9novis9@gmail.com>
2023-09-29 22:43:33 -0300

    align

--- src/routes/root/root.jsx
+++ src/routes/root/root.jsx
@@ -6,15 +6,15 @@ export default function Root() {
         <>
             <div id="sidebar">
                 <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
-                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                        <S.Ul className="navbar-nav ml-auto">
-                            <li class="nav-item active ">
+                    <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            <li class="nav-item active">
                                 <S.NavLink className="nav-link" to={`/sign-in`}>Sign In</S.NavLink>
                             </li>
                             <li class="nav-item">
                                 <S.NavLink className="nav-link" to={`/sign-up`}>Sign Up</S.NavLink>
                             </li>
-                        </S.Ul>
+                        </ul>
                     </div>
                 </nav>
             </div>
